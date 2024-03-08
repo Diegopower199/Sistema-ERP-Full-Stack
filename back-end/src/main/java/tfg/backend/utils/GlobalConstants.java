0ea936946d781245e3c3231bdf9d29a01d5fa2fd
Diegopower199 <diegogs2323@gmail.com>
2024-03-08 12:05:16 +0100

    Limpiar caché de Git y volver a agregar archivos

--- back-end/src/main/java/tfg/backend/utils/GlobalConstants.java
+++
@@ -1,9 +0,0 @@
-package tfg.backend.utils;
-
-public class GlobalConstants {
-
-    public static final String FRONTEND_URL = "http://localhost:3000";
-    public static final String HOST_BLOCKCHAIN_SERVER = "localhost";
-    public static final int PORT_BLOCKCHAIN_SERVER = 12345;
-
-}

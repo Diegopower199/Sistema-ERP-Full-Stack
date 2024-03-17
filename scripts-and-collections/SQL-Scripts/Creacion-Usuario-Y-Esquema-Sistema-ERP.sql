bf423d781d568f0f4ab9fa3d0554a5286a8a13fe
Diegopower199 <diegogs2323@gmail.com>
2024-03-17 14:48:13 +0100

    update README.md

--- scripts-and-collections/SQL-Scripts/Creacion-Usuario-Y-Esquema-Sistema-ERP.sql
+++ scripts-and-collections/SQL-Scripts/Creacion-Usuario-Y-Esquema-Sistema-ERP.sql
@@ -2,11 +2,9 @@ DROP USER IF EXISTS 'diegogonzalez'@'localhost';
 
 CREATE USER 'diegogonzalez'@'localhost' IDENTIFIED WITH mysql_native_password BY 'bbdd1122';
 
-
 DROP SCHEMA IF EXISTS trabajotfgerp;
 
 CREATE SCHEMA trabajotfgerp;
-
 
 GRANT ALL PRIVILEGES ON trabajotfgerp.* TO 'diegogonzalez'@'localhost';
 
